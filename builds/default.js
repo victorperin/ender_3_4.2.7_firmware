3a5f1d9613694f31e25535539d8a8dd01f810c68
Victor Perin <me@victorperin.ninja>
2022-12-14 20:48:45 -0300

    bugfix(builds/default): folder not found

--- builds/default.js
+++ builds/default.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/Creality/Ender-3/CrealityV4.2.7/",
+        path: "/config/examples/Creality/Ender-3/CrealityV427/",
         stable_branch: "release-{{marlin_version}}",
         nightly_branch: "bugfix-2.1.x"
     }
