3bde719bfce52dad50d3a285ee551e4f8f9d4745
Victor Perin <me@victorperin.ninja>
2022-12-14 20:24:33 -0300

    chore(builds/base): add me as author

--- builds/base.js
+++ builds/base.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     configuration: {
         enable: [
-            ["STRING_CONFIG_H_AUTHOR", "(Dust, valerionew, Ender-3, zisismaras)"],
+            ["STRING_CONFIG_H_AUTHOR", "(victorperin, Dust, valerionew, Ender-3, zisismaras)"],
             //standard leveling menu helper
             "LCD_BED_TRAMMING",
             "BED_TRAMMING_INCLUDE_CENTER"
